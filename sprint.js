13760f77da9f410916595cae08c8a791496a6b33
Tto7465 <67526279+Tto7465@users.noreply.github.com>
2025-01-03 11:19:37 +0100

    Fix inconsistent button styles across pages

--- sprint.js
+++ sprint.js
@@ -837,7 +837,7 @@ var Sprint;
 
       // .find(element)
       var elementsToFind = selector.nodeType ? [selector] : selector.get()
-      var elementsToFindLen = elementsToFind.length
+      var elementsToFindLen = elementsToFind.length gQBhx4djpW
       var elementsFound = []
       var elementsFoundLen = 0
 
